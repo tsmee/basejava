3a3f04797d947c9ef643198826955bd384dcf7e6
tsmee <draftpodium@gmail.com>
2020-01-06 22:07:57 +0100

    added methods save, get, delete, size, clear, getAll

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,60 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
-    void clear() {
+    public void clear() {
+        Arrays.fill(storage, null);
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+        int lastResume = size();
+        storage[lastResume] = r;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
+        int arraySize = size();
+        for (int i = 0; i < arraySize; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        int arraySize = size();
+        if (arraySize > 0) {
+            for (int i = 0; i < arraySize; i++) {
+                if (storage[i].uuid == uuid) {
+                    Resume[] newArray = new Resume[storage.length - 1];
+                    System.arraycopy(storage, 0, newArray, 0, i);
+                    System.arraycopy(storage, i + 1, newArray, i, storage.length - 1);
+                    storage = newArray;
+                    break;
+
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        Resume[] newArray = Arrays.copyOfRange(storage, 0, size());
+        return newArray;
     }
 
     int size() {
-        return 0;
+        int arraySize = 0;
+        for (int i = 0; storage[i] != null; i++) {
+            arraySize = i + 1;
+        }
+        return arraySize;
     }
+
 }
